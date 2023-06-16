66840629fcbf5f04eba981fa25f9f78e6c51cf31
Eduardo Guette <eduardoguette@gmail.com>
2023-06-16 19:29:12 +0200

    add  project section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,22 @@
- 
- const Projects = ({projects}) => {
-  return <div>
-    <h2>Projects</h2>
-    <ul className="flex md:flex-row gap-2">
-      {projects.map((project, index) => (
-        <li className="border p-3 border-gray-500 rounded-md" key={index}>
-          <image src={project.image} alt={project.name} />
-          <h3 className="font-medium">{project.name}</h3>
-          <p>{project.description}</p>
-        </li>
-      ))}
-    </ul>
-  </div>;
-};
+const Projects = ({ projects }) => {
+  return (
+    <div>
+      <h2>Projects</h2>
+      <ul className="flex md:flex-row gap-2">
+        {projects.map((project, index) => (
+          <li className="border p-3 border-gray-500 rounded-md" key={index}>
+            <img
+              src={project.image}
+              alt={project.name}
+              referrerPolicy="no-referrer"
+            />
+            <h3 className="font-medium">{project.name}</h3>
+            <p>{project.description}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
-export default Projects;
+export default Projects
