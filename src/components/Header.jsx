112497af2093155f87afc0b31bbce6b07f356a5d
Eduardo Guette <eduardoguette@gmail.com>
2023-06-18 11:43:56 +0200

    add meta and favicon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,19 @@
 
 const Header = () => {
   return (
-    <header class="flex justify-between items-center py-4 mb-16 relative z-10">
-      <h1 class="font-native text-2xl">
-        <span class="font-bold underline decoration-wavy decoration-indigo-600">
+    <header className="flex justify-between items-center py-4 mb-16 relative z-10">
+      <h1 className="font-native text-2xl">
+        <span className="font-bold underline decoration-wavy decoration-indigo-600">
           EG.
         </span>
       </h1>
 
-      <nav class="flex items-center gap-2 text-gray-800">
-        <a href="/" class="hover:text-indigo-600">
+      <nav className="flex items-center gap-2 text-gray-800">
+        <a href="/" className="hover:text-indigo-600">
           EN
         </a>
         <span>{'/'}</span>
-        <a href="/es" class="hover:text-indigo-600">
+        <a href="/es" className="hover:text-indigo-600">
           ES
         </a>
       </nav>
