2849094ef7a886d4f2d4183c7df92080b9c98328
Eduardo Guette <eduardoguette@gmail.com>
2023-06-19 21:56:16 +0200

    add fonts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,15 @@
-
 const Header = () => {
+  const string = '{'
+  const string2 = '}'
   return (
-    <header className="flex justify-between items-center py-4 mb-16 relative z-10">
-      <h1 className="font-native text-2xl">
-        <span className="font-bold underline decoration-wavy decoration-indigo-600">
-          EG.
-        </span>
-      </h1>
+    <header className="flex justify-between items-center py-4 mb-16 relative z-10 header">
+      <div className="max-content">
+        <h1 className="font-bold font-mono text-xl">
+          {string}
+          <span className="text-indigo-500">EduardoGuette</span>
+          {string2}
+        </h1>
+      </div>
 
       <nav className="flex items-center gap-2 text-gray-800">
         <a href="/" className="hover:text-indigo-600">
--- src/components/TitileSection.jsx
+++ src/components/TitileSection.jsx
@@ -2,7 +2,7 @@ export const TitileSection = ({ title, id }) => {
   return (
     <div>
       <h2
-        className="text-2xl font-bold underline decoration-wavy decoration-indigo-600 my-4"
+        className="text-2xl font-bold text-gray-700 my-4"
         id={id}
       >
         {title}
