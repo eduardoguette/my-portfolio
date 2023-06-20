e5bfa36ab12e8f11724524f16facc304e04945bf
Eduardo Guette <eduardoguette@gmail.com>
2023-06-20 22:57:50 +0200

    remove fonts

--- src/components/Colaborators.jsx
+++ src/components/Colaborators.jsx
@@ -6,6 +6,8 @@ export const Colaborators = ({ colaborator }) => {
         alt={colaborator.name}
         className="w-8 h-8 rounded-full inline-block border-2 border-white aspect-square"
         referrerPolicy="no-referrer"
+        height={32}
+        width={32}
         title={colaborator.name}
       />
     </a>
