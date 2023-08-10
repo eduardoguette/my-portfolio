3dba68cbd8b30d3bdba485402abc02a7399e8ba3
Eduardo Guette <eduardoguette@gmail.com>
2023-08-10 20:42:36 +0200

    fix: date in roles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
-import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from "@astrojs/tailwind"
+import vercel from '@astrojs/vercel/edge'
 
-import react from "@astrojs/react";
+import { defineConfig } from 'astro/config'
+
+import react from "@astrojs/react"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel({
+    analytics: true,
+  }),
 });
